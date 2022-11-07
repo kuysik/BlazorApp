00eaeb6b6968f270a9c6d0c0b3777ff2e7b17e16
onlyttani <onlyttani@naver.com>
2022-11-08 05:37:38 +0900

    first commit

---
+++ Todo/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorApp.Todo
+{
+    public class TodoItem
+    {
+        //?를 사용하여 Title을 nullable 문자열로 선언합니다.
+        public string? Title { get; set; }
+        public bool IsDone { get; set; } = false;
+    }
+}
